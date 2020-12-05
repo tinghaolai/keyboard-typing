d14b6a81e59198163077aa0a0eca9999573c6d41
Ting-Hao Lai <analth355@gmail.com>
2020-12-05 22:51:44 +0800

    First version of page to typing practice page

--- resources/views/typing/index.blade.php
+++ resources/views/typing/index.blade.php
@@ -1,4 +1,16 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/element-ui@2.13.0/lib/theme-chalk/index.css">
+<style>
+    #typingText.el-input__inner {
+        background-color: #C0C4CC;
+    }
+
+    #typingText {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        z-index: 1000;
+    }
+</style>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
@@ -8,8 +20,23 @@
 </head>
 <body>
     <div id="app">
-        <div>複製文章</div>
+        <div v-if="(displayText) && (displayText.length > 0)">
+            Typing area
+            <div v-if="typingIndex < displayText.length">現在要打的字 @{{ displayText[typingIndex] }} <span v-show="typeResult">, @{{ typeResult }}</span></div>
+            <div v-else>結束</div>
+            <div style="background-color: #303133;margin: 10px; white-space: pre-line">
+                <span style="color: #67C23A">@{{ displayText.slice(0, typingIndex) }}</span>
+                <span :style="currentTypingStyle" id="targetChar">@{{ displayText[typingIndex] }}</span>
+                <span style="color: #F2F6FC">@{{ displayText.slice(typingIndex+1, displayText.length) }}</span>
+            </div>
+        </div>
+        <div style="margin: 10px">
+            <el-input v-model="typingText" id="typingText" ref="typingText" @input="handleTypingTextChanged"></el-input>
+        </div>
+        <span>複製文章</span>
+        <el-button type="danger" plain @click="resetType">Reset</el-button>
         <el-input
+            @input="handleCopyTextChanged"
             type="textarea"
             :rows="20"
             v-model="copyText">
@@ -27,16 +54,61 @@
     document.write("<script async src='/browser-sync/browser-sync-client.2.10.1.js'><\/script>".replace("HOST", location.hostname));
     //]]></script>
 <script>
-    console.log(1234);
     new Vue({
         el: '#app',
         data() {
             return {
-                copyText: '123',
+                typeResult: null,
+                typingText: '',
+                copyText: '',
+                displayText: '',
+                typingIndex: 0,
+                currentTypingStyle: 'color: #E6A23C',
             }
         },
         mounted() {
-          console.log('mounted');
+            window.addEventListener("keypress", e => {
+                this.focusInput();
+            });
+        },
+        methods: {
+            handleCopyTextChanged(value) {
+                this.displayText = value;
+                this.focusInput();
+            },
+            handleTypingTextChanged(value) {
+                value = value.slice(value.length - 1);
+                if (this.displayText.length > 0) {
+                    if (this.typingIndex > this.displayText.length) {
+                        this.typeResult ='無法再輸入更多';
+                        return;
+                    }
+
+                    if (value.toLowerCase() === this.displayText[this.typingIndex].toLowerCase()) {
+                        this.typeResult = null;
+                        this.currentTypingStyle = 'color: #E6A23C';
+                        do {
+                            this.typingIndex += 1;
+                            if (this.typingIndex === this.displayText.length) {
+                                this.typeResult ='全部輸入完成';
+                                return;
+                            }
+                        } while (!this.displayText[this.typingIndex].match(/^[A-Za-z]+$/));
+                    } else {
+                        this.typeResult = '錯誤，你輸入: ' + value;
+                        this.currentTypingStyle = 'color: #F56C6C';
+                    }
+                }
+            },
+            focusInput() {
+                this.$refs.typingText.focus();
+            },
+            resetType() {
+                this.currentTypingStyle = 'color: #E6A23C';
+                this.typeResult = null;
+                this.typingIndex = 0;
+                this.focusInput();
+            }
         },
     });
 </script>
